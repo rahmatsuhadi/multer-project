69d191e845c834d551d0dc804d8f28f048639add
Rahmat S <rahmat@carakan.id>
2024-07-07 19:26:02 +0700

    update-configuration

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const bodyParser= require('body-parser');
 const app = express();
+
+const fs = require('fs');
+const path = require('path');
 app.use(bodyParser.urlencoded({extended: true}))
+
 //ROUTES WILL GO HERE
 var uploadRouter = require('./routes/upload');
+
 app.use('/', uploadRouter);
+
+app.get('/list', (req, res) => {
+    const directoryPath = path.join(__dirname, './public/uploads'); // Adjust the path as per your directory structure
+    // Baca isi direktori 'upload'
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            console.error('Error reading directory:', err);
+            return res.status(500).send('Error reading directory');
+        }
+        // Kirim daftar file sebagai respons
+        res.json({ files });
+    });
+});
  
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(3001, () => console.log('Server started on port 3000'));
--- routes/upload.js
+++ routes/upload.js
@@ -1,44 +1,56 @@
 var express = require('express');
 var router = express.Router();
-var multer  = require('multer');
- 
+var multer = require('multer');
+var path = require("path")
+
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/uploads/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now()+file.originalname)
-    }
-  })
- 
-  const fileFilter=(req, file, cb)=>{
-   if(file.mimetype ==='image/jpeg' || file.mimetype ==='image/jpg' || file.mimetype ==='image/png'){
-       cb(null,true);
-   }else{
-       cb(null, false);
-   }
- 
+  destination: function (req, file, cb) {
+    // cb(null, "./public/uploads")
+
+    cb(null, path?.join(__dirname, "../public/uploads"))
+  },
+  // filename: function (req, file, cb) {
+  //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+  //   cb(null, file.fieldname + '-' + uniqueSuffix)
+  // }
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+  },
+  
+})
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
   }
- 
-var upload = multer({ 
-    storage:storage,
-    limits:{
-        fileSize: 1024 * 1024 * 5
-    },
-    fileFilter:fileFilter
- });
- 
-router.get('/',function(req,res){
-  res.sendFile(__dirname + '/index.html');
- 
+
+}
+
+var upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  
+  // fileFilter: fileFilter
 });
-router.post("/fileupload",upload.single('image'),function(req,res,next){
-   
-const filename=req.file.filename;
- res.json({
-            message:"Image Uploaded Successfully",
-            filename:filename
-        });
-    });
- 
-module.exports=router;
+
+router.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+
+});
+router.post("/fileupload", upload.single('image'), function (req, res, next) {
+
+  const file = req?.file?.path;
+  if(!file){
+    res.status(400).json({status: false, data: "no file is selected"})
+  }
+
+  res.send(file)
+
+
+});
+
+module.exports = router;
